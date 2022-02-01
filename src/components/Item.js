910d1efa250bb2a6fa1dc4a79f043820ef75ce8f
batuhan <avcibatuhan26@gmail.com>
2022-02-01 14:22:26 +0300

    state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+
+  const addItem = ()=>{
+    setCart(true);
+    
+  }
+
   return (
-    <li className="">
+    <li className={cart ? 'in-cart': ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addItem} className="add">Add to Cart</button>
     </li>
   );
 }
